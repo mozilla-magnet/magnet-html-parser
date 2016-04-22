fbd5559dab64e17ccec74d173cd4ddf194b4115a
Wilson Page <wilsonpage@me.com>
2016-04-22 17:23:00 +0100

    Added .parse() method to allow for sinon stubbing

--- index.js
+++ index.js
@@ -3,14 +3,27 @@
  * Dependencies
  */
 
-var parse = require('./lib/parse');
+var exec = require('./lib/parse');
 var cheerio = require('cheerio');
 
 /**
  * Exports
  */
 
-module.exports = function(html, url) {
+module.exports = parse;
+
+// providing an optional method interface allows
+// users to easily stub it out for testing
+module.exports.parse = parse;
+
+/**
+ * Parse an html document string.
+ *
+ * @param  {String} html
+ * @param  {String} baseUrl
+ * @return {Promise<Object>}
+ */
+function parse(html, baseUrl) {
   if (!html) return Promise.resolve(null);
-  return parse(cheerio.load(html), url);
+  return exec(cheerio.load(html), baseUrl);
 };
